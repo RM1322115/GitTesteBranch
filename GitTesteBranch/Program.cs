20b7afa7092a1499a208dbebcb3595e1259e4da3
everton@admdeveloper.com.br <everton@admdeveloper.com.br>
2021-08-12 14:19:40 -0300

    Adicionar arquivos de projeto.

---
+++ GitTesteBranch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTesteBranch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
